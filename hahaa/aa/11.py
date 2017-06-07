2f8adf654c63841a79723513386e8e1f5437ff5a
weiyang <296075945@qq.com>
2017-06-07 09:21:33 +0800

    11

--- hahaa/aa/11.py
+++ hahaa/aa/11.py
@@ -5,9 +5,11 @@ import base64
 def encodeUid(uid):
     return base64.b64encode(uid).replace("M", "$").replace("D", "!").replace("2", "*")
 
-print encodeUid('50131')
-headers = {'Content-Type': 'application/json','ukey':encodeUid('50131')}
+#print encodeUid('50131')
+headers = {'Content-Type': 'application/json','ukey':encodeUid('50131'),'appVersion':'1.0.0'}
 data={}
 rr=requests.post('http://60.205.228.129:8080/small/question/problemList/0',data=json.dumps(data),headers=headers)
 print (rr.text)
 print (rr.json()['code'])
+
+
--- hahaa/aa/interface.py
+++ hahaa/aa/interface.py
@@ -1,18 +1,21 @@
 import requests
 import json
 
-url = "http://localhost:8080/cc/"
+# url = "http://localhost:8080/cc/"
 headers  ={'Content-Type': 'application/json'}
-print requests.post(url+"head",headers ={'name':'weiyang00'}).text
+# print requests.post(url+"head",headers ={'name':'weiyang00'}).text
+# 
+# print requests.post(url+"body",data=json.dumps({'123':"123"}),
+#                     headers =headers).text
+#                     
+# print requests.post(url+"body",data=json.dumps([{'name':'hhd'},{'name':'wy'}]),
+#                     headers =headers).text
+#                     
+# print requests.post(url+"body",
+#                     headers =headers).text
+#                     
+# print requests.post(url+"cookie",
+#                     cookies={"JSESSIONID":'12312',"userName":"nidaye"}).text 
 
-print requests.post(url+"body",data=json.dumps({'123':"123"}),
-                    headers =headers).text
-                    
-print requests.post(url+"body",data=json.dumps([{'name':'hhd'},{'name':'wy'}]),
-                    headers =headers).text
-                    
-print requests.post(url+"body",
-                    headers =headers).text
-                    
-print requests.post(url+"cookie",
-                    cookies={"JSESSIONID":'12312',"userName":"nidaye"}).text             
+print requests.post("http://localhost:8090/login",data=json.dumps({'userName':'hhd','password':'wy'}),
+                    headers =headers,timeout=100).text                                
